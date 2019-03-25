e5b6bb9d4d67eee3ec5ae05ce7965115b6610a12
Austin Johnson <me@austinj.me>
2019-03-24 19:56:55 -0500

    implement image effects

--- src/shaders/interface_fs.rs
+++ src/shaders/interface_fs.rs
@@ -70,46 +70,27 @@ pub mod interface_fs {
 				
 				out_color = vec4(r, g, b, tex_color.a);
 			} else if(type == 102) { // BackColorAdd
-			
+				out_color = clamp(textureBicubic(coords) + color, 0.0, 1.0);
 			} else if(type == 103) { // BackColorBehind
-			
+				vec4 image_color = textureBicubic(coords);
+				out_color = clamp(mix(color, image_color, image_color.z), 0.0, 1.0);
 			} else if(type == 104) { // BackColorSubtract
-			
+				out_color = clamp(textureBicubic(coords) - color, 0.0, 1.0);
 			} else if(type == 105) { // BackColorMultiply
-			
+				out_color = clamp(textureBicubic(coords) * color, 0.0, 1.0);
 			} else if(type == 106) { // BackColorDivide
-			
+				out_color = clamp(textureBicubic(coords) / color, 0.0, 1.0);
 			} else if(type == 107) { // Invert
-			
-			} else {
+				vec4 image_color = textureBicubic(coords);
+				out_color = vec4(
+					1.0 - image_color.r,
+					1.0 - image_color.g,
+					1.0 - image_color.b,
+					image_color.a
+				);
+			} else { // Normal Image / Unknown
 				out_color = textureBicubic(coords);
 			}
-		}
-		
-		
-		else if(type == 2) { // Verts with Texture
-			vec4 tex_color = textureBicubic(coords);
-			out_color = vec4(mix(tex_color.rgb, color.rgb, color.a), tex_color.a);
-		} else if(type == 3) { // YUV & SRGB
-			vec2 y_coords = vec2(coords.x, (coords.y / 3.0) * 2.0);
-			vec2 u_coords = vec2(coords.x / 2.0, (2.0 / 3.0) + (coords.y / 3.0));
-			vec2 v_coords = vec2(0.5 + (coords.x / 2.0), (2.0 / 3.0) + (coords.y / 3.0));
-			
-			vec4 tex_color = vec4(
-				textureBicubic(y_coords).r,
-				textureBicubic(u_coords).r,
-				textureBicubic(v_coords).r,
-				1.0
-			);
-			
-			float r = tex_color.r + (1.402 * (tex_color.b - 0.5));
-			float g = tex_color.r - (0.344 * (tex_color.g - 0.5)) - (0.714 * (tex_color.b - 0.5));
-			float b = tex_color.r + (1.772 * (tex_color.g - 0.5));
-			r = pow((r + 0.055) / 1.055, 2.4);
-			g = pow((g + 0.055) / 1.055, 2.4);
-			b = pow((b + 0.055) / 1.055, 2.4);
-			
-			out_color = vec4(r, g, b, tex_color.a);
 		} else { // Unknown
 			out_color = color;
 		}
